8b0c82a0d74e0dff25c353f9da7daec29d67b4ca
mschoeffel <michael.schoeffel@t-online.de>
2020-12-02 11:37:41 +0100

    Day 02 Part 2 + Refactoring

--- src/main/java/de/mschoeffel/Day02/Day02App.java
+++ src/main/java/de/mschoeffel/Day02/Day02App.java
@@ -16,8 +16,8 @@ public class Day02App {
             char c = inputLine.substring(inputLine.indexOf(' ') + 1, inputLine.indexOf(':')).charAt(0);
             String password = inputLine.substring(inputLine.indexOf(':') + 2);
 
-            if (isPasswordCorrect(min, max, c, password)) {
-                System.out.printf("[Day 02 Part 1] Valid Password Found: %s\n", password);
+            if (isPasswordOccurrCorrect(min, max, c, password)) {
+                System.out.printf("[Day 02 Part 1] Valid Password Found: Occurr[%s-%s] Char[%s] %s\n", min, max, c, password);
                 correctPasswords++;
             }
         }
@@ -25,7 +25,7 @@ public class Day02App {
         return correctPasswords;
     }
 
-    public static boolean isPasswordCorrect(int min, int max, char c, String password) {
+    public static boolean isPasswordOccurrCorrect(int min, int max, char c, String password) {
         char[] passwordChars = password.toCharArray();
         int occurrences = 0;
 
@@ -38,12 +38,36 @@ public class Day02App {
         return (occurrences >= min && occurrences <= max);
     }
 
+    public static int day02Part2(List<String> inputLines) {
+        int correctPasswords = 0;
+
+        for (String input : inputLines) {
+            int posOne = Integer.parseInt(input.substring(0, input.indexOf('-')));
+            int posTwo = Integer.parseInt(input.substring(input.indexOf('-') + 1, input.indexOf(' ')));
+            char c = input.substring(input.indexOf(' ') + 1, input.indexOf(':')).charAt(0);
+            String password = input.substring(input.indexOf(':') + 2);
+
+            if (isPasswordPosCorrect(posOne, posTwo, c, password)) {
+                System.out.printf("[Day 02 Part 2] Valid Password Found: Pos[%s, %s] Char[%s] %s\n", posOne, posTwo, c, password);
+                correctPasswords++;
+            }
+        }
+
+        return correctPasswords;
+    }
+
+    public static boolean isPasswordPosCorrect(int posOne, int posTwo, char c, String password) {
+        return ((password.charAt(posOne - 1) == c && password.charAt(posTwo - 1) != c)
+                || (password.charAt(posOne - 1) != c && password.charAt(posTwo - 1) == c));
+    }
+
     public static void main(String[] args) {
         String inputPath = "Day02/input.txt";
         try {
             List<String> inputs = Utils.readFileLineByLine(inputPath);
             System.out.printf("[Day 02 Part 1] [Result]: %s\n", day02Part1(inputs));
             System.out.println();
+            System.out.printf("[Day 02 Part 2] [Result]: %s\n", day02Part2(inputs));
         } catch (IOException e) {
             System.out.println("[Day 02] [Error]: Error reading input.txt: " + e);
         }
