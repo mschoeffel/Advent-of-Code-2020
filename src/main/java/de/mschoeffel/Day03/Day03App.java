15378207707205cf65f71e7c12f2f7fdd6dcbf81
mschoeffel <michael.schoeffel@t-online.de>
2020-12-03 11:35:46 +0100

    Day 03 Part 2

--- src/main/java/de/mschoeffel/Day03/Day03App.java
+++ src/main/java/de/mschoeffel/Day03/Day03App.java
@@ -8,12 +8,18 @@ import java.util.List;
 public class Day03App {
 
     public static int day03Part1(List<String> inputLines) {
+        int trees = countTrees(1, 3, inputLines);
+        System.out.printf("[Day 03 Part 1] Trees found 1 Down 3 Right: %s\n", trees);
+        return trees;
+    }
+
+    private static int countTrees(int down, int right, List<String> inputLines) {
         int trees = 0;
         int xPos = 0;
 
-        for (int i = 1; i < inputLines.size(); i++) {
+        for (int i = down; i < inputLines.size(); i += down) {
             String inputLine = inputLines.get(i);
-            xPos += 3;
+            xPos += right;
             if (xPos >= inputLine.length()) {
                 xPos -= inputLine.length();
             }
@@ -25,12 +31,32 @@ public class Day03App {
         return trees;
     }
 
+    public static long day03Part2(List<String> inputLines) {
+        int trees1Down1Right = countTrees(1, 1, inputLines);
+        System.out.printf("[Day 03 Part 2] Trees found 1 Down 1 Right: %s\n", trees1Down1Right);
+
+        int trees1Down3Right = countTrees(1, 3, inputLines);
+        System.out.printf("[Day 03 Part 2] Trees found 1 Down 3 Right: %s\n", trees1Down3Right);
+
+        int trees1Down5Right = countTrees(1, 5, inputLines);
+        System.out.printf("[Day 03 Part 2] Trees found 1 Down 5 Right: %s\n", trees1Down5Right);
+
+        int trees1Down7Right = countTrees(1, 7, inputLines);
+        System.out.printf("[Day 03 Part 2] Trees found 1 Down 7 Right: %s\n", trees1Down7Right);
+
+        int trees2Down1Right = countTrees(2, 1, inputLines);
+        System.out.printf("[Day 03 Part 2] Trees found 2 Down 1 Right: %s\n", trees2Down1Right);
+
+        return (long) trees1Down1Right * (long) trees1Down3Right * (long) trees1Down5Right * (long) trees1Down7Right * (long) trees2Down1Right;
+    }
+
     public static void main(String[] args) {
         String inputPath = "Day03/input.txt";
         try {
             List<String> inputs = Utils.readFileLineByLine(inputPath);
             System.out.printf("[Day 03 Part 1] [Result]: %s\n", day03Part1(inputs));
             System.out.println();
+            System.out.printf("[Day 03 Part 2] [Result]: %s\n", day03Part2(inputs));
         } catch (IOException e) {
             System.out.println("[Day 03] [Error]: Error reading input.txt: " + e);
         }
