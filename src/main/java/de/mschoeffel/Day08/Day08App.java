733951bdf9b01ffc55173c52d6a61a26499da0f4
mschoeffel <michael.schoeffel@t-online.de>
2020-12-08 07:31:56 +0100

    Day 08 Part 2

--- src/main/java/de/mschoeffel/Day08/Day08App.java
+++ src/main/java/de/mschoeffel/Day08/Day08App.java
@@ -19,6 +19,7 @@ public class Day08App {
             int argument = Integer.parseInt(inputLineSplit[1]);
 
             if (visitedLines.contains(i)) {
+                System.out.printf("[Day 08 Part 2] Program has already executed line: %s\n", i);
                 return acc;
             }
             visitedLines.add(i);
@@ -38,12 +39,65 @@ public class Day08App {
         return acc;
     }
 
+    public static int day08Part2(List<String> inputLines) {
+        Set<Integer> changedLines = new HashSet<>();
+        while (true) {
+            boolean changePossible = true;
+            int acc = 0;
+            Set<Integer> visitedLines = new HashSet<>();
+
+            System.out.printf("[Day 08 Part 2] New attempt Changes tried: %s\n", changedLines);
+
+            for (int i = 0; i < inputLines.size(); i++) {
+                String[] inputLineSplit = inputLines.get(i).split(" ");
+                String operation = inputLineSplit[0];
+                int argument = Integer.parseInt(inputLineSplit[1]);
+
+                if (visitedLines.contains(i)) {
+                    System.out.printf("[Day 08 Part 2] Program has already executed line: %s\n", i);
+                    break;
+                }
+                visitedLines.add(i);
+
+                if (changePossible && !changedLines.contains(i)) {
+                    if (operation.equals("jmp")) {
+                        System.out.printf("[Day 08 Part 2] Changing line: %s From: jmp %s to nop %s\n", i, argument, argument);
+                        operation = "nop";
+                        changedLines.add(i);
+                        changePossible = false;
+                    } else if (operation.equals("nop")) {
+                        System.out.printf("[Day 08 Part 2] Changing line: %s From: nop %s to jmp %s\n", i, argument, argument);
+                        operation = "jmp";
+                        changedLines.add(i);
+                        changePossible = false;
+                    }
+                }
+
+                switch (operation) {
+                    case "acc":
+                        acc += argument;
+                        break;
+                    case "jmp":
+                        i += argument - 1;
+                        break;
+                    case "nop":
+                        break;
+                }
+                if (i >= inputLines.size() - 1) {
+                    System.out.printf("[Day 08 Part 2] Program finished successfully. Reached Line: %s\n", i + 1);
+                    return acc;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String inputPath = "Day08/input.txt";
         try {
             List<String> inputs = Utils.readFileLineByLine(inputPath);
             System.out.printf("[Day 08 Part 1] [Result]: %s\n", day08Part1(inputs));
             System.out.println();
+            System.out.printf("[Day 08 Part 2] [Result]: %s\n", day08Part2(inputs));
         } catch (IOException e) {
             System.out.println("[Day 08] [Error]: Error reading input.txt: " + e);
         }
