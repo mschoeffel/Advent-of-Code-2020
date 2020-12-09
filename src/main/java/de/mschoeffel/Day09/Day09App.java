b29756231d20dfe8da1997afdf8b092f2c3f380c
mschoeffel <michael.schoeffel@t-online.de>
2020-12-09 10:19:02 +0100

    Day 09 Part 2

--- src/main/java/de/mschoeffel/Day09/Day09App.java
+++ src/main/java/de/mschoeffel/Day09/Day09App.java
@@ -8,12 +8,16 @@ import java.util.List;
 public class Day09App {
 
     public static int day09Part1(List<String> inputLines) {
+        return findInvalidNumber(inputLines);
+    }
+
+    private static int findInvalidNumber(List<String> inputLines) {
         for (int i = 25; i < inputLines.size(); i++) {
             int toFind = Integer.parseInt(inputLines.get(i));
             if (isValid(inputLines, i, toFind)) {
-                System.out.printf("[Day 09 Part 1] Valid Number: %s\n", toFind);
+                System.out.printf("[Day 09 Part 1 OR 2] Valid Number: %s\n", toFind);
             } else {
-                System.out.printf("[Day 09 Part 1] Invalid Number found: %s\n", toFind);
+                System.out.printf("[Day 09 Part 1 OR 2] Invalid Number found: %s\n", toFind);
                 return toFind;
             }
         }
@@ -35,12 +39,53 @@ public class Day09App {
         return false;
     }
 
+    public static long day09Part2(List<String> inputLines) {
+        int invalidNumber = findInvalidNumber(inputLines);
+        int minIndex = 0;
+        int maxIndex = 0;
+        boolean found = false;
+
+        for (int i = 0; i < inputLines.size() && !found; i++) {
+            minIndex = i;
+            long currentSum = Long.parseLong(inputLines.get(i));
+            for (int j = i + 1; j < inputLines.size() && !found; j++) {
+                maxIndex = j;
+                long number = Long.parseLong(inputLines.get(j));
+                System.out.printf("[Day 09 Part 2] Checking numbers from: %s to: %s current sum: %s\n", minIndex, maxIndex, currentSum + number);
+                if (number + currentSum == invalidNumber) {
+                    System.out.printf("[Day 09 Part 2] Valid List found from min: %s to max: %s sum: %s\n", minIndex, maxIndex, currentSum + number);
+                    found = true;
+                } else if (number + currentSum > invalidNumber) {
+                    break;
+                } else {
+                    currentSum += number;
+                }
+            }
+        }
+        System.out.printf("[Day 09 Part 2] Searching for min value and max value in index range min: %s max:%s\n", minIndex, maxIndex);
+        long min = Long.MAX_VALUE;
+        long max = 0;
+        for (int i = minIndex; i <= maxIndex; i++) {
+            int currentNumber = Integer.parseInt(inputLines.get(i));
+            if (currentNumber > max) {
+                System.out.printf("[Day 09 Part 2] New max value found: old: %s new: %s\n", max, currentNumber);
+                max = currentNumber;
+            }
+            if (currentNumber < min) {
+                System.out.printf("[Day 09 Part 2] New min value found: old: %s new: %s\n", min, currentNumber);
+                min = currentNumber;
+            }
+        }
+        return min + max;
+    }
+
     public static void main(String[] args) {
         String inputPath = "Day09/input.txt";
         try {
             List<String> inputs = Utils.readFileLineByLine(inputPath);
             System.out.printf("[Day 09 Part 1] [Result]: %s\n", day09Part1(inputs));
             System.out.println();
+            System.out.printf("[Day 09 Part 2] [Result]: %s\n", day09Part2(inputs));
         } catch (IOException e) {
             System.out.println("[Day 09] [Error]: Error reading input.txt: " + e);
         }
