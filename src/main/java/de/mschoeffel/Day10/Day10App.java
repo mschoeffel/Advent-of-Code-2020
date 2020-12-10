33a003cee20f416580d26eb22d32dd0c15484842
mschoeffel <michael.schoeffel@t-online.de>
2020-12-10 13:12:09 +0100

    Day 10 Part 2

--- src/main/java/de/mschoeffel/Day10/Day10App.java
+++ src/main/java/de/mschoeffel/Day10/Day10App.java
@@ -4,6 +4,7 @@ import de.mschoeffel.Utils.Utils;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -35,12 +36,48 @@ public class Day10App {
         return one * three;
     }
 
+    public static long day10Part2(List<String> inputLines) {
+        List<Integer> number = new ArrayList<>();
+        for (String num : inputLines) {
+            number.add(Integer.parseInt(num));
+        }
+        number.add(0);
+        number.sort(Integer::compareTo);
+
+        int len = number.size();
+        long[] dp = new long[len];
+        Arrays.fill(dp, -1);
+
+        return calcWays(number, 0, dp);
+    }
+
+    public static long calcWays(List<Integer> numbers, int index, long[] dp) {
+        if (index == numbers.size() - 1) {
+            return 1;
+        } else {
+            if (dp[index] != -1) {
+                return dp[index];
+            } else {
+                long count = 0;
+                for (int i = index + 1; i <= Math.min(index + 3, numbers.size() - 1); i++) {
+                    if (numbers.get(index) + 3 >= numbers.get(i)) {
+                        count += calcWays(numbers, i, dp);
+                    }
+                }
+                dp[index] = count;
+                return count;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         String inputPath = "Day10/input.txt";
         try {
             List<String> inputs = Utils.readFileLineByLine(inputPath);
             System.out.printf("[Day 10 Part 1] [Result]: %s\n", day10Part1(inputs));
             System.out.println();
+            System.out.printf("[Day 10 Part 2] [Result]: %s\n", day10Part2(inputs));
         } catch (IOException e) {
             System.out.println("[Day 10] [Error]: Error reading input.txt: " + e);
         }
